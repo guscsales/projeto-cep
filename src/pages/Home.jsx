7a116bbb1ecd17491dbd91f72948370eda8cf33c
Gustavo Sales <gustavo@gsales.io>
2023-06-27 22:03:31 -0300

    atualização de texto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <Container className="flex flex-col justify-center items-center mt-28">
       <header className="flex flex-col items-center gap-1">
         <Text as="h1" variation="4xl" className="text-center">
-          Busque um endereço...
+          Busque qualquer endereço...
         </Text>
         <Text as="p" className="text-gray-300 italic text-center">
           Digite o valor do CEP do endereço para coletar mais informações sobre
